91855582a99c2e882eb0bf35d4ce04f5800e12c5
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-11-08 07:18:21 -0300

    :construction: abstract criteria

---
+++ src/main/java/org/example/ch02/Section0201.java
@@ -0,0 +1,20 @@
+package org.example.ch02;
+
+import org.example.ex010302.Apple;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Section0201 {
+    public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
+        List<Apple> result = new ArrayList<>();
+
+        for (Apple apple : inventory) {
+            if (p.test(apple))
+                result.add(apple);
+        }
+
+        return result;
+    }
+}
